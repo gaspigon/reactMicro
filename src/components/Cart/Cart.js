229c922df0862cb9b4f53c379963bb0a18be7591
gasparg <gaspi-gonzalez@hotmail.com>
2022-06-11 23:16:39 -0300

    carro

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,10 +9,20 @@ const Cart = () => {
         <div>
             <h1>Cart</h1>
             <div>
-                {cart.map(prod => <div key={prod.id}>{prod.name}</div>)}
-            </div>
+                {cart.map(prod => {
+                    return(
+                        <div>
+                         <div key={prod.id}> {prod.name} </div>
+                                <div>Cantidad</div>
+                                <div>Precio</div>
+                                <div>Subtotal</div>
+                        </div>
+                            )})
+                }
+            </div> 
+        
         </div>
-    )
+     )
 }
 
 export default Cart
--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -24,11 +24,10 @@ const Itemcount = ({stock,onConfirm}) => {
     return(
         <div >
             <div className='box-count'>
-                {/* <button onClick ={decrement}>-</button> */}
-                <ButtonCount handleClick ={decrement} label='-' color='red'/> 
+                <ButtonCount handleClick={decrement} label='-' color='red'/>  
                 <p className='number'>{count}</p>
-                {/* <button onClick ={increment}>+</button> */}
-                <ButtonCount handleClick={increment} label='+' color='green' /> 
+              
+                 <ButtonCount handleClick={increment} label='+' color='green' />  
              </div>
             <div>
                 <button className='btn-agregar' onClick={() => onConfirm(count)}>Agregar a carrito</button>
