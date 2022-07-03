2fe696713fc6c09d9654cd2789ed8eaf40f1b0b7
gasparg <gaspi-gonzalez@hotmail.com>
2022-07-03 15:41:08 -0300

    errores

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React, {   useState } from "react";
+import {useForm} from "react-hook-form"
 import '../Form/Form.css'
 
 
@@ -6,7 +7,8 @@ import '../Form/Form.css'
 
 const FormData = ({setShowForm, setBuyerData, setDisable}) => {
 
-
+  const {register, formState:{errors}, handleSubmit} = useForm();
+ 
 
   const [buyer, setBuyer] = useState({
     name: '',
@@ -16,19 +18,21 @@ const FormData = ({setShowForm, setBuyerData, setDisable}) => {
 
 
   const submitData = (e) => {
-    e.preventDefault()
+    
     setBuyerData(buyer)
     setShowForm(false)
     setDisable(false)
   }
 
   return (
-      <form className="form" onSubmit={submitData}>
+      <form className="form" onSubmit={(handleSubmit(submitData)) }>
         <div className="box-form">
-          <input type={'text'} placeholder='nombre' value={buyer.name} onChange={(e) => setBuyer({...buyer, name: e.target.value})} />
-          <input type={'email'} placeholder='email' value={buyer.email} onChange={(e) => setBuyer({...buyer, email: e.target.value})}/>
-          <input type={'number'} placeholder='celular' value={buyer.phone} onChange={(e) => setBuyer({...buyer, phone: e.target.value})} />
-          <input type={'submit'} />
+          <h3 className="form-title">Complete sus datos de contacto!</h3>
+          <input type={'text'} className="form-input" placeholder="Nombre" {...register("Nombre", {required:true , pattern:{ value: ""} })} value={buyer.name} onChange={(e) => setBuyer({...buyer, name: e.target.value})} />
+          {errors.Nombre && <span style={{fontSize:12, color:'red'}}>Campo Requerido</span>}
+          <input type={'email'} className="form-input" placeholder='Email' value={buyer.email} onChange={(e) => setBuyer({...buyer, email: e.target.value})}/>
+          <input type={'number'} className="form-input" placeholder='Celular' {...register("Celular", {required:true , pattern:{ value: ""} })} value={buyer.phone} onChange={(e) => setBuyer({...buyer, phone: e.target.value})} />
+          <input type={'submit'} className="form-submit" />
           
         </div>
         
